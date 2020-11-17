b137840abe9079bc2b8f635729e88a1d5fef886e
Radamés Ajna <radamajna@gmail.com>
2020-11-17 15:36:12 -0800

    Initial commit

---
+++ post.config.js
@@ -0,0 +1,21 @@
+export default {
+  baseURL: process.env.BASE_URL_PRODUCTION || '/doing-it-for-the-gram',
+  title: 'Doing It For The ‘Gram',
+  author: 'Zoe Harwood',
+  publishDate: 'Oct. 2, 2019',
+  location: 'Oakland, CA',
+  description:
+    'People have been showing their support for Black Lives Matter on TikTok, Twitter, and Instagram, but do social media posts help the movement?',
+  tweetMessage: 'with @itsyrmedia',
+  url: 'https://interactive.yr.media/doing-it-for-the-gram/',
+  featureImage: 'https://interactive.yr.media/doing-it-for-the-gram/social.jpg',
+  featureImagePath: 'images/template-feature-image',
+  featureImageDescription: 'People Protesting BLM',
+  featureImageCaption: '(Photo: Andersen Ross Photography Inc/Getty Images)',
+
+  wpPostSlug: 'north-carolina-vs-vaping-companies',
+  wpPostID: '60986',
+  // you might not need to change
+  fbAppID: '73080818131',
+  twitterHandler: '@itsyrmedia',
+}
