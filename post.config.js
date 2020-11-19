1c290704fa17c37d18db65c744a4ea7fde3e1b3a
Radames Ajna <radamajna@gmail.com>
2020-11-19 13:25:31 -0800

    google docs fetch scripts

--- post.config.js
+++ post.config.js
@@ -1,6 +1,6 @@
-export default {
-  baseURL: process.env.BASE_URL_PRODUCTION || '/doing-it-for-the-gram',
-  title: 'Doing It For The ‘Gram',
+module.exports = {
+  baseURL: process.env.BASE_URL_PRODUCTION || '/deepfake-quiz',
+  title: 'DeepFake Quiz',
   author: 'Zoe Harwood',
   publishDate: 'Oct. 2, 2019',
   location: 'Oakland, CA',
@@ -18,4 +18,11 @@ export default {
   // you might not need to change
   fbAppID: '73080818131',
   twitterHandler: '@itsyrmedia',
+  docs: [
+    {
+      name: 'Deepfake writing assignment - backend',
+      id: '19ZB-U9LdMPbIFjT9RJ8HBh8fCoQnKzoJCDnus9tK_j0',
+    },
+  ],
+  dataPath: 'data/data.json',
 }
